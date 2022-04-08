3da2d595f030ebefe5d8b0c9a4b94d9ce30ebae1
Steph Paterson <steph.c.paterson@gmail.com>
2022-04-08 16:37:28 +0100

    created basic error handling and continue play option

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# run this file in the terminal
-from xml.etree.ElementTree import tostring
 from computer_player import ComputerPlayer
 from player import Player
 from game import Game
@@ -16,17 +14,27 @@ def find_by_index(choices, player_str):
 
 
 player_name = input("What's your name? ")
-print("Choose your hand...")
-display_choices(choices)
 
-player_str = input("What's the number of your choice? ")
-player_choice = find_by_index(choices, player_str)
+while True:
+    
+    print("Choose your hand...")
+    display_choices(choices)
+    player_input = int(input("What's the number of your choice? "))
+    if player_input not in range(1,6):
+        print("-----------")
+        print("Sorry you need to pick a number bewteen 1 and 5")
+        print("-----------")
+    else:
+        player_choice = find_by_index(choices, player_input)
 
-player_1 = Player(player_name, player_choice)
-computer = ComputerPlayer()
-computer.choice_randomiser()
-game = Game(player_1, computer)
+        player_1 = Player(player_name, player_choice)
+        computer = ComputerPlayer()
+        computer.choice_randomiser()
+        game = Game(player_1, computer)
 
-print(f"The computer chose {computer.choice} and {player_1.name} chose {player_1.choice}")
-print(game.result())
+        print(f"The computer chose {computer.choice} and {player_1.name} chose {player_1.choice}")
+        print(game.result())
+        play_again = input("would you like to play again? (y/n)")
+        if play_again.lower() == "n":
+            break
 
